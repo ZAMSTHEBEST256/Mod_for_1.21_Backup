acd5b29686af570e72502c5a58a3d028c8305fb6
jojob <hangubodo@gmail.com>
2024-08-19 02:17:38 +0530

    Block of Pink Garnet and Block of Raw Pink Garnet added

--- src/main/java/net/samanyu/tutorialmod/TutorialMod.java
+++ src/main/java/net/samanyu/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.samanyu.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.samanyu.tutorialmod.block.ModBlocks;
 import net.samanyu.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
